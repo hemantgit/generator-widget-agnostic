5e83490a0dd524fbb640ef8319c897d639049cbf
Tomas Corral <tomas@backbase.com>
2016-01-12 15:32:22 +0100

    Add namespace

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // Add your Javascript code.
-be = be || {};
-be.widgets = be.widgets || {};
-be.widgets['${widget.name}'] = {
+${widget.namespace} = ${widget.namespace} || {};
+${widget.namespace}.widgets = ${widget.namespace}.widgets || {};
+${widget.namespace}.widgets['${widget.name}'] = {
     init: function (widgetInstance) {
         console.log(widgetInstance.name);
     }
